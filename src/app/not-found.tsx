fea0dc6a8a626990778b8c5b653015ff6bda2a0a
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-13 19:36:17 +0700

    refactor: tweak page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,25 @@
 export default function NotFound() {
   return (
-    <div>
-      <p className="text-lg">Page Not Found</p>
-    </div>
+    <>
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            Page not found
+          </h1>
+          <p className="mt-6 text-base leading-7">
+            Sorry, we couldn&apos;t find the page you&apos;re looking for.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            >
+              Go back home
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
