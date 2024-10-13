00b4102fb353c2e6444718a15ec7fe533e255858
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-14 00:40:10 +0700

    feat: add prefetch

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -22,6 +22,7 @@ export default function NotFound() {
             <Link
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
               href="/"
+              prefetch
             >
               Go back home
             </Link>
