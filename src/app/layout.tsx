503ec2a0c542e6e40c01395a1ecd870aff6a6d58
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-14 22:57:29 +0700

    style: mistyping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
