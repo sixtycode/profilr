830f005b8b57639df7453241604a5ee4e610204a
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-14 14:59:54 +0700

    refactor: dark improvement

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -20,7 +20,7 @@ export default function NotFound() {
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
-              className="rounded-full border border-solid transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] hover:text-white dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              className="rounded-full border border-solid transition-colors flex items-center justify-center bg-black dark:bg-transparent text-white dark:text-white gap-2 hover:bg-[#383838] hover:text-white dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
               href="/"
             >
               Go back home
