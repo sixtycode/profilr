a518d1e0f25ad577a7230809ecf31f37e2165bd8
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-14 23:48:01 +0700

    feat: add page navbar

---
+++ src/app/services/page.tsx
@@ -0,0 +1,17 @@
+export default function Services() {
+  return (
+    <>
+      <main className="grid min-h-full place-items-center bg-white dark:bg-neutral-800 px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold">Services</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            Vehicle For Today
+          </h1>
+          <p className="mt-6 text-base leading-7">
+            Explore Latest or even Vintage! because why not?
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
