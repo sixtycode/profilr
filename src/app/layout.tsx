ebb595924ef9a1240add4872ec3cbc60dfcc8031
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-14 11:29:19 +0700

    refactor: layout dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
@@ -11,12 +14,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className="bg-white dark:bg-slate-950">{children}</body>
     </html>
   );
 }
