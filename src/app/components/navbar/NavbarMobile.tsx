35b93060fe758b15751a2e4460bb2f28b3909f32
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-15 15:21:27 +0700

    refactor: tweak navbar responsiveness

--- src/app/components/navbar/NavbarMobile.tsx
+++ src/app/components/navbar/NavbarMobile.tsx
@@ -7,7 +7,7 @@ export default function NavbarMobile() {
       <nav className="bg-black text-white dark:bg-neutral-300 dark:text-black sticky shadow-md">
         <Link
           href="/"
-          className="md:hidden block flex flex-1 items-center justify-center sm:items-stretch sm:justify-start h-14 w-auto font-mono font-extrabold text-3xl"
+          className="md:hidden sm:hidden block flex flex-1 items-center justify-center sm:items-stretch sm:justify-start h-14 w-auto font-mono font-extrabold text-3xl"
         >
           VHICL
         </Link>
@@ -16,7 +16,7 @@ export default function NavbarMobile() {
             <Link
               href={item.link}
               key={item.link}
-              className="md:hidden block h-12 pl-4 py-2 hover:font-semibold dark:hover:font-semibold"
+              className="md:hidden sm:hidden block h-12 pl-4 py-2 hover:font-semibold dark:hover:font-semibold"
             >
               {item.name}
             </Link>
