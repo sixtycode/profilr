ffc19f6063b5e30b179c053f907def0ebe97b657
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-15 14:31:59 +0700

    style: link color

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         2024{" "}
         <Link
           href="https://github.com/sixtycode/profilrsix"
-          className="text-blue-800 dark:text-blue-100 hover:text-blue-900 dark:hover:text-blue-200 visited:text-purple-800 dark:visited:text-purple-200 hover:underline hover:underline-offset-4"
+          className="text-blue-600 dark:text-blue-100 hover:text-blue-800 dark:hover:text-blue-200 visited:text-purple-600 dark:visited:text-purple-200 hover:underline hover:underline-offset-4"
         >
           sixtycode
         </Link>
