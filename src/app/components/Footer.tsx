a9f4427c444f32655808592a2d44a8b8ce7a3ede
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-15 14:34:28 +0700

    style: underline

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <footer className="text-black text-center text-sm p-3 pb-20 md:pb-3">
+      <footer className="text-black dark:text-white text-center text-sm p-3 pb-20 md:pb-3">
         <span>© </span>
         2024{" "}
         <Link
           href="https://github.com/sixtycode/profilrsix"
-          className="text-blue-600 dark:text-blue-100 hover:text-blue-800 dark:hover:text-blue-200 visited:text-purple-600 dark:visited:text-purple-200 hover:underline hover:underline-offset-4"
+          className="text-blue-600 dark:text-blue-100 hover:text-blue-800 dark:hover:text-blue-200 visited:text-purple-600 dark:visited:text-purple-200 underline underline-offset-4"
         >
           sixtycode
         </Link>
