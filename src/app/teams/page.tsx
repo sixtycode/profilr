a4030e00becba10747dcae387a3e7ca1f8bd68f5
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-15 13:49:57 +0700

    style: accessibility

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,7 +1,7 @@
 export default function Teams() {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white dark:bg-neutral-800 px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold">Teams</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
