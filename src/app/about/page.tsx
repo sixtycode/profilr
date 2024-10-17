cad33f678f3bbc79aee72a908f063af690c92de1
sixtycode <sagaraperkasa.work@gmail.com>
2024-10-18 03:30:48 +0700

    feat: About Us Page Completed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,46 @@
 import { Metadata } from "next";
+import RandomUser from "../api/random-user";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "About Us",
   description: "About Company Profiles",
 };
 
-export default function About() {
+export default async function About() {
+  const aboutData = await RandomUser(4, "nomail");
+
+  const expTeam = [
+    {
+      name: `${aboutData.results[0].name.first} ${aboutData.results[0].name.last}`,
+      role: "Chief Technology Officer",
+      experience:
+        "Working at VHICL is a fun Roller Coaster Thrilling Experience but in a good way.",
+      imageUrl: aboutData.results[0].picture.large,
+    },
+    {
+      name: `${aboutData.results[1].name.first} ${aboutData.results[1].name.last}`,
+      role: "Chief Operating Officer",
+      experience:
+        "Been VHICL Company through 3 years, i see no regrets at all even We gain solid achievement.",
+      imageUrl: aboutData.results[1].picture.large,
+    },
+    {
+      name: `${aboutData.results[2].name.first} ${aboutData.results[2].name.last}`,
+      role: "Human Resource Development",
+      experience:
+        "Altough Hiring Process Tiring Process, after get the job done i usually play with each others, Hard Work Pays Off and still couting 7 years goes on.",
+      imageUrl: aboutData.results[2].picture.large,
+    },
+    {
+      name: `${aboutData.results[3].name.first} ${aboutData.results[3].name.last}`,
+      role: "Product Manager",
+      experience:
+        "Product Manager literally managing product, and after deliver seeing happy VHICL customers really delightful experience.",
+      imageUrl: aboutData.results[3].picture.large,
+    },
+  ];
+
   return (
     <>
       <main className="grid place-items-center px-8 py-8 sm:py-8 lg:px-4">
@@ -20,7 +55,7 @@ export default function About() {
           <p className="my-4 text-lg font-mono">
             &quot;Starts with Curiousity, Drives with Passion.&quot;
           </p>
-          <p className="text-left my-8 px-10 text-justify">
+          <p className="my-8 px-10 text-justify">
             Founding: Idea Sparks at Sagara Garage, like what if we create media
             stream based on daily hobby in this case Vehicle, even though it is
             just a little amount of information, at very least we contribute to
@@ -33,7 +68,7 @@ export default function About() {
             everybody participating even overseas bro still connected to until
             today.
           </p>
-          <p className="text-left mt-8 px-10 text-justify">
+          <p className="mt-8 px-10 text-justify">
             Milestones: 2014-2015 just gain 347 subscriber and yes one year
             later we still at garage without official office very broke but
             surprisingly miracle happens. 2016-2019 sweet years ever where we
@@ -48,18 +83,36 @@ export default function About() {
             finally hit 100k! moral of the story keep strong enough whatever
             situation hitting us and smile.
           </p>
-          <p className="mt-8 mb-4 text-2xl tracking-tight font-extrabold">
+          <p className="py-8 text-2xl tracking-tight font-extrabold">
             VHICL&apos;s Team Members
           </p>
-          <h1>
-            Team: This section should introduce the company&apos;s team members.
-            It should include their names, titles, and a brief description of
-            their experience.
-          </h1>
+          <div className="px-10 grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+            {expTeam.map((vhteam) => (
+              <div
+                key={vhteam.name}
+                className="items-center rounded-lg shadow sm:flex"
+              >
+                <Image
+                  className="w-full md:w-auto rounded-lg sm:rounded-none sm:rounded-l-lg"
+                  src={vhteam.imageUrl}
+                  alt={vhteam.name}
+                  width={128}
+                  height={128}
+                />
+                <div className="p-5">
+                  <h3 className="text-xl font-bold tracking-tight">
+                    {vhteam.name}
+                  </h3>
+                  <span>{vhteam.role}</span>
+                  <p className="mt-3 mb-4 font-light">{vhteam.experience}</p>
+                </div>
+              </div>
+            ))}
+          </div>
           <p className="mt-8 mb-4 text-2xl tracking-tight font-extrabold">
             VHICL&apos;s Culture
           </p>
-          <p className="text-left px-10 text-justify">
+          <p className="px-10 text-justify">
             Innovate with passion, engage with purpose and win with integrity.
             To live up to these values, the company encourages its team to find
             a healthy and sustainable work-life balance through wellness days,
